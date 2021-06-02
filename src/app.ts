1363c7b14c5497077d6aba8119e6d017df2a8009
Jimmy Zhang <jimmy.zhang@microsoft.com>
2021-06-02 12:27:27 -0700

    may top world builders update

--- src/app.ts
+++ src/app.ts
@@ -187,33 +187,33 @@ Enter a search term and click "OK"
 
     // create favorites board
     // => created from the buttom up
-    // UPDATED: 2021-05-05
+    // UPDATED: 2021-06-02
     let worldBuilders: string[][] = [
-      ["Darren", "VRDarrenG"],
       ["Daisy Shaw", "BestBearEver"],
-      ["tajasuka", "tajasuka"],
-      ["Kbot", "kvanalstine"],
-      ["Lightyear (Scott) World Builder/Musician", "scottfin767"],
+      ["Darren", "VRDarrenG"],
       ["Matty Boy", "MattyBoy"],
+      ["tajasuka", "tajasuka"],
+      ["Karnivore", "markgill47"],
+      ["Kbot", "kvanalstine"],
+      ["{Mr. Disney}", "TheVoiceGuy1"],
+      ["Artsy (MC)", "artsy"],
       ["SHUSHU", "HOLODRAMA"],
       ["Andy", "djvivid"],
+      ["VRMax Technologies Solutions", "VRMaxTech"],
       ["Mr. Prime", "OctopusPrime"],
-      ["Luis Neo Buda", "LuisNeoBuda"],
-      ["{The Voice Guy}", "TheVoiceGuy1"],
-      ["Karnivore", "markgill47"],
-      ["Doug - BRCvr )*(", "Dougj11"],
-      ["Niko", "nikolang88"],
-      ["Artsy (MC)", "artsy"],
-      ["OptiC", "OptiC_AltspaceVR"],
-      ["byfaithtoday", "byfaithtoday"],
-      ["Nera", "whimwhams"],
-      ["Simsimer", "Simsimer"],
-      ["((Wookie Picard))âs Other Avatar", "WabiSabi777"],
-      [")'( Icewiz", "Jim_Ottens"],
-      ["MsPeeps", "proudlove44"],
-      ["Alan #stopaapihate #blm", "acd"],
-      ["Blaze", "Ablazekiller101"],
+      ["Lightyear (Scott) World Builder/Musician", "scottfin767"],
+      ["MOLLY (Queen of Hearts)", "GypsySoul"],
       ["James", "Velox"],
+      ["Nera", "whimwhams"],
+      ["Hazel", "DuncanEyes"],
+      ["Luis Neo Buda", "LuisNeoBuda"],
+      ["Terry", "TerryVallery"],
+      ["Doug - BRCvr )*(", "Dougj11"],
+      ["HartmanVR", "Hartman747"],
+      ["OptiC", "OptiC_AltspaceVR"],
+      ["Evgeniya", "Evinewyork"],
+      ["Olly", "Olly"],
+      ["WALLY", "WALLY1987"],
     ].reverse();
     let button = null;
     for(var user of worldBuilders){
